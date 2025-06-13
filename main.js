d94d7aac9a952b56f73bf5258824b266f883ffb5
SantiagoRomero7 <santiagoinstagram23@gmail.com>
2025-06-13 20:03:02 +0200

    feat: :sparkles: mapeo de imágenes locales para personajes
    se agrego mapeo de imágenes locales para personajes

---
+++ main.js
@@ -0,0 +1,17 @@
+const API_URL = "https://swapi.py4e.com/api/";
+
+
+const characterImages = {
+  "Luke Skywalker": "./skywalker.jpg",
+  "Darth Vader": "./darthvader.jpg",
+  "Leia Organa": "./leiaorgana.jpg",
+  "Obi-Wan Kenobi": "./obiwan.jpg",
+  "C-3PO": "./c3po.jpg",
+  "R2-D2": "./r2d2.jpg",
+  "R5-D4": "./r5d4.jpg",
+  "Owen Lars": "./owenlars.jpg",
+  "Beru Whitesun lars": "./beru.jpg",
+  "Biggs Darklighter": "./biggs.jpg",
+  
+};
+
